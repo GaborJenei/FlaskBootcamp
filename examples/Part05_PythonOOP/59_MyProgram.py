7a587f60b209ca2f3fb3b12a1f642d646252a683
GaborJenei <jeneigabor14@gmail.com>
2022-01-19 19:43:12 +0000

    Add 59 Python modules and packages

---
+++ examples/Part05_PythonOOP/59_MyProgram.py
@@ -0,0 +1,10 @@
+from MyModule import my_func
+
+my_func()
+
+# Import from a package
+from my_main_package import some_main_script
+from my_main_package.subpackage import mysubscript
+
+some_main_script.report_main()
+mysubscript.sub_report()
