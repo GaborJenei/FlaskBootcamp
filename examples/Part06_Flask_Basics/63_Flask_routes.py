9b41daf20879ca8f01b6cec840f0ac67ac3ab261
GaborJenei <jeneigabor14@gmail.com>
2022-01-20 16:25:52 +0000

    Add 63 Flask routes

---
+++ examples/Part06_Flask_Basics/63_Flask_routes.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello Puppy</h1>'
+
+
+@app.route('/information')
+def info():
+    return '<h2>Puppies are cute</h2>'
+
+
+if __name__ == '__main__':
+    app.run()
