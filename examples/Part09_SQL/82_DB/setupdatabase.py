48806784156d4023dd49b9589287ac6ca9d73314
Gabor Jenei <jeneigabor14@gmail.com>
2022-01-25 20:02:09 +0000

    Add 86 Database add items

---
+++ examples/Part09_SQL/82_DB/setupdatabase.py
@@ -0,0 +1,22 @@
+from app import db, Puppy
+
+# create all the tables
+db.create_all()
+
+# We can create new entries now
+
+sam = Puppy('Sammy', 3)
+frank = Puppy('Frankie', 4)
+
+# None
+# None
+print(sam.id)
+print(frank.id)
+
+# add all item from the list
+db.session.add_all([sam, frank])
+
+# alternatively add one: db.session.add(sam)
+
+print(sam.id)
+print(frank.id)
