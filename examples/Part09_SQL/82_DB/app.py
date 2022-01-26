1a62e52602e173be198f4cba7e20f46072ed4a70
Gabor Jenei <jeneigabor14@gmail.com>
2022-01-26 08:55:46 +0000

    Add 84 Flask Migrate

--- examples/Part09_SQL/82_DB/app.py
+++ examples/Part09_SQL/82_DB/app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # Grab the filename and the directory name
 # And create an absolute path
@@ -15,6 +16,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Connect flask migrate
+Migrate(app, db)
+# After this we need to set the environment variable FLASK_APP=app.py or whatever the main app python file is called
+# on Win set FLASK_APP=app.py
+# on Linux export FLASK_APP=app.py
+
 
 # set up a table
 class Puppy(db.Model):
@@ -24,10 +31,12 @@ class Puppy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
+    breed = db.Column(db.Text)
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
+        self.breed = breed
 
     # This will be printed
     def __repr__(self):
