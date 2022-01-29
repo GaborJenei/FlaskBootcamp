d8e34d6014c5125468d61e9820d0b25936eea7fc
Gabor Jenei <jeneigabor14@gmail.com>
2022-01-29 19:12:35 +0000

    Add 97 Password hashing with bcrypt update

--- examples/Part11_UserAuth/97_UserAuth/crypt.py
+++ examples/Part11_UserAuth/97_UserAuth/crypt.py
@@ -8,3 +8,5 @@ hashed = bcrypt.generate_password_hash(password=password)
 
 print(hashed)
 
+check = bcrypt.check_password_hash(hashed, '$2b$12$PM/5cMRXP0OXzvhJ5IH7BO1DE5jF2KIH4NNFAOVVQqpXL/M6z4O3K')
+print(check)
