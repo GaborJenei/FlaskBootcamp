522c53019de2d3ff6b139d600ff38f7a4335b7d1
Gabor Jenei <jeneigabor14@gmail.com>
2022-02-02 18:31:16 +0000

    Add 109 User data model

--- examples/Part13_BlogProject/puppycompanyblog/__init__.py
+++ examples/Part13_BlogProject/puppycompanyblog/__init__.py
@@ -1,9 +1,22 @@
-
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 
 
+# Database setup
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog_db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
+# registering Views using Blueprints
 from puppycompanyblog.core.views import core
 from puppycompanyblog.error_pages.handlers import error_pages
 
