9b0c130d69ff19045232e303b02ea00a91efe265
Gabor Jenei <jeneigabor14@gmail.com>
2022-02-09 18:01:33 +0000

    Add 112 user account and user blog page views

--- examples/Part13_BlogProject/puppycompanyblog/users/views.py
+++ examples/Part13_BlogProject/puppycompanyblog/users/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
 
-from models import User
+from models import User, BlogPost
 from puppycompanyblog import db
 from puppycompanyblog.users.forms import RegistrationForm, LoginForm, UpdateUserForm
 from puppycompanyblog.users.picture_handler import add_profile_pic
@@ -32,7 +32,6 @@ def register():
 # login view
 @users.route('/login', methods=['GET', 'POST'])
 def login():
-
     form = LoginForm()
 
     if form.validate_on_submit():
@@ -46,7 +45,7 @@ def login():
 
             next = request.args.get('next')
 
-            if next is None or not next[0]=='/':
+            if next is None or not next[0] == '/':
                 next = url_for('core.index')
 
             return redirect(next)
@@ -60,6 +59,41 @@ def logout():
     logout_user()
     return redirect(url_for('core.index'))
 
+
 # Account update
+@users.route('/route', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateUserForm()
+    if form.validate_on_submit():
+
+        if form.picture.data:
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data, username)
+            current_user.profile_image = pic
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+
+        db.session.commit()
+
+        flash('user account updated')
+        return redirect(url_for('users.account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    profile_image = url_for('static',
+                            filename='profile_pics/' + current_user.profile_pic)
+    return render_template('account.html', profile_image=profile_image, form=form)
+
 
 # Blogpost - user's list of blogpost
+@users.route("/<username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int) # to show blogposts on pages
+    user = User.query.filter_by(username=username).first_or_404()
+    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+
+    return render_template('user_blog_posts.html', blog_posts=blog_posts, user=user)
