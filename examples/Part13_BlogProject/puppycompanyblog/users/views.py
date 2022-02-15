25f91621fba2cba54975d723d856174e518acad7
Gabor Jenei <jeneigabor14@gmail.com>
2022-02-15 22:17:30 +0000

    BUGFIX Typos causing app not to build

--- examples/Part13_BlogProject/puppycompanyblog/users/views.py
+++ examples/Part13_BlogProject/puppycompanyblog/users/views.py
@@ -3,6 +3,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 
 from models import User, BlogPost
 from puppycompanyblog import db
+from puppycompanyblog.models import User, BlogPost
 from puppycompanyblog.users.forms import RegistrationForm, LoginForm, UpdateUserForm
 from puppycompanyblog.users.picture_handler import add_profile_pic
 
