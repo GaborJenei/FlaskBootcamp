7752c0665ab0fd06a0c01388dc865c7d7aea2ba1
Gabor Jenei <jeneigabor14@gmail.com>
2022-02-23 22:20:23 +0000

    bugfix

--- examples/Part13_BlogProject/puppycompanyblog/blogposts/views.py
+++ examples/Part13_BlogProject/puppycompanyblog/blogposts/views.py
@@ -4,11 +4,11 @@ from puppycompanyblog import db
 from puppycompanyblog.models import BlogPost
 from puppycompanyblog.blogposts.forms import BlogPostForm
 
-blog_post = Blueprint('blogposts', __name__)
+blog_posts = Blueprint('blog_posts', __name__)
 
 
 # Create
-@blog_post.route('/create', methods=['GET','POST'])
+@blog_posts.route('/create', methods=['GET','POST'])
 @login_required
 def create_post():
     form = BlogPostForm()
@@ -30,7 +30,7 @@ def create_post():
 
 
 # Read Posts
-@blog_post.route('/<int:blog_post_id>')
+@blog_posts.route('/<int:blog_post_id>')
 def blog_post(blog_post_id):
 
     blog_post = BlogPost.query.get_or_404(blog_post_id)
@@ -42,7 +42,7 @@ def blog_post(blog_post_id):
 
 
 # Update post
-@blog_post.route('/<int:blog_post_id>/update', methods=['GET','POST'])
+@blog_posts.route('/<int:blog_post_id>/update', methods=['GET','POST'])
 @login_required
 def update(blog_post_id):
 
@@ -76,7 +76,7 @@ def update(blog_post_id):
 
 
 # Delete post
-@blog_post.route('/<int:blog_post_id>/delete', methods=['GET','POST'])
+@blog_posts.route('/<int:blog_post_id>/delete', methods=['GET','POST'])
 @login_required
 def delete_post(blog_post_id):
 
