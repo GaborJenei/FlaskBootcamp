1c83012f634f5deda0e7e619dbde7cfb32d6b197
Gabor Jenei <jeneigabor14@gmail.com>
2022-02-26 22:01:22 +0000

    add 121 crud rest basics

---
+++ examples/Part14_API/121_crud_rest_basics/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+puppies = []
+
+
+class PuppyNames(Resource):
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def delete(self):
+        pass
