b4d89d416906a8efa925f1480f9c21be06bc2b03
Gabor Jenei <jeneigabor14@gmail.com>
2022-02-27 09:59:33 +0000

    update 121 crud rest basics

--- examples/Part14_API/121_crud_rest_basics/app.py
+++ examples/Part14_API/121_crud_rest_basics/app.py
@@ -4,17 +4,43 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
+# list of dictionaries
 puppies = []
 
 
+# get, post, delete all have to have the same parameters
 class PuppyNames(Resource):
 
+    def get(self, name):
+
+        for pup in puppies:
+            if pup['name'] == name:
+                return pup
+
+        return {'name': None}, 404
+
+    def post(self, name):
+
+        pup = {'name': name}
+        puppies.append(pup)
+        return pup
+
+    def delete(self, name):
+
+        for ind, pup in enumerate(puppies):
+            if pup['name'] == name:
+                deleted_pup = puppies.pop(ind)
+                return {'note':'delete success'}
+
+
+class AllNames(Resource):
+
     def get(self):
-        pass
+        return {'puppies': puppies}
 
-    def post(self):
-        pass
 
-    def delete(self):
-        pass
+api.add_resource(PuppyNames, '/puppy/<string:name>')
+api.add_resource(AllNames, '/puppies')
+
+if __name__ == '__main__':
+    app.run(debug=True)
