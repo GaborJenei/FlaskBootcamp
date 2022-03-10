3f84e4da742cda28a944c34ab1150fe6431b2fcf
Gabor Jenei <jeneigabor14@gmail.com>
2022-03-10 08:07:35 +0000

    add padding to pictures and href to cloud bucket

--- practice/01_wedding/app.py
+++ practice/01_wedding/app.py
@@ -5,14 +5,12 @@ import os
 app = Flask(__name__)
 
 
-# https://firebasestorage.googleapis.com/v0/b/wedding-pics-83add.appspot.com/o/1003%20B24I7821.JPG?token=6d7b3677-2818-445d-9f20-0dffb2a80e8c
-# gs://wedding-pics-83add.appspot.com/1003 B24I7821.JPG?token=6d7b3677-2818-445d-9f20-0dffb2a80e8c
-
 # Read up file names
 file_names = []
-root = 'gs://wedding-pics-83add.appspot.com/'
+root = 'link to bucket'
+
 for file in os.listdir('./static'):
-    file_names.append(file)
+    file_names.append(root + file.replace(' ', '%20'))
 
 
 @app.route('/')
